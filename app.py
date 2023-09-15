54866e1d18c918e46a0e4023f489420aeb523cef
Zain Merchant <merchantzain@users.noreply.github.com>
2023-09-14 22:56:46 -0400

    basic application working now

--- app.py
+++ app.py
@@ -1,13 +1,47 @@
 # import libraries
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import openai
+import os
+from base64 import b64decode
+from pathlib import Path
 
 # define flask application (invoked by wsgi)
 app = Flask(__name__)
+
+# obtain openai key
+openai.api_key = os.environ["AWDS_OPENAI_KEY"]
 
 # home page
 @app.route("/")
 def hello_world():
     return render_template('index.html')
+
+# get name ideas for the new pokemon
+@app.route('/get_inspiration', methods=['GET', 'POST'])
+def get_inspiration():
+    # process input query
+    data = request.get_json()
+    processed_colors = f"{data['colors'][0]} and {data['colors'][1]}" if len(data['colors']) > 1 else data['colors'][0]
+    processed_types = f"{data['types'][0]} and {data['types'][1]}" if len(data['types']) > 1 else data['types'][0]
+    names_query = f"Give a 5 names for a {processed_types} type Pokemon that lives in {data['habitat']} and resembles a {data['animal']} in the format 1. name"
+    image_query = f"{processed_colors} {processed_types} type Pokemon that lives in {data['habitat']} and resembles a {data['animal']}"
+    
+    # make request for obtaining ideas for possible names
+    names_response = openai.Completion.create(engine="text-davinci-003", prompt=names_query, max_tokens=256)["choices"][0]["text"]
+    names_response = names_response.split('\n')
+    names_parsed = [line.split('. ')[1].split(' ')[0] for line in names_response if len(line.split('. ')) == 2]
+    
+    # make a request for an illustration example
+    image_url = openai.Image.create(
+        prompt=image_query,
+        n=1,
+        size="512x512",
+    )['data'][0]['url']
+    
+    return jsonify({
+        "names": names_parsed,
+        "image_url": image_url
+    })
 
 # start flask application on script run
 if __name__ == "__main__":
--- static/script.js
+++ static/script.js
@@ -1,3 +1,8 @@
+// hide results on startup
+document.querySelectorAll(".result").forEach(function(result) {
+    result.style.display = 'none';
+});
+
 // handle clicking button selectors for colors and type
 function toggleColorButton(button) {
     button.classList.toggle("clicked");
@@ -13,7 +18,7 @@ function toggleColorButton(button) {
 function toggleTypeButton(button) {
     button.classList.toggle("clicked");
     
-    const clickedButtons = document.querySelectorAll(".type_btn.clicked");
+    const clickedButtons = document.querySelectorAll(".type_btn.clicked")
     if (clickedButtons.length > 2) {
         clickedButtons.forEach((btn) => {
             btn.classList.remove("clicked");
@@ -21,4 +26,57 @@ function toggleTypeButton(button) {
     }
 }
 
-// 
+// handle generate button actions
+function generateButton() {
+    // collect data
+    let animal = document.getElementById("looks_like").value
+    let habitat = document.getElementById("habitat").value
+    let colors = Array.from(document.querySelectorAll('.color_btn.clicked'), element => element.textContent.trim())
+    let types = Array.from(document.querySelectorAll('.type_btn.clicked'), element => element.textContent.trim())
+
+    if (!animal || !habitat || colors.length < 1 || types.length < 1) {
+        alert("Some data has been left incomplete. Please input and continue.")
+        return
+    }
+
+    // pass input to backend and handle response
+    let data = {
+        "animal": animal,
+        "habitat": habitat,
+        "colors": colors,
+        "types": types
+    }
+    $.ajax({
+        type: "POST",
+        url: "/get_inspiration",                
+        dataType : "json",
+        contentType: "application/json; charset=utf-8",
+        data : JSON.stringify(data),
+        beforeSend: function () {
+            $("#spinner-div").show()
+        },
+        success: function(data){
+            // display new image
+            document.querySelectorAll(".result").forEach(function(result) {
+                document.getElementById("pokemon-pic").src = data["image_url"]
+                result.style.display = 'block';
+            });
+
+            // display possible names
+            var name_list = ""
+            for (const name of data["names"]) {
+                name_list += "<li>" + name + "</li>\n"
+            }
+            document.getElementById("names_generated").innerHTML = name_list
+        },
+        error: function(request, status, error) {
+            console.log("Error");
+            console.log(request)
+            console.log(status)
+            console.log(error)
+        },
+        complete: function () {
+            $("#spinner-div").hide()
+        },
+    })
+}
